d8dec63e0588e396ae6ecbdd53b4de5f1d242601
221RDB101 Dominiks Vaivods <76108496+Dominiks-tech@users.noreply.github.com>
2023-03-19 19:24:35 +0000

    darbs

--- build_heap.py
+++ build_heap.py
@@ -1,13 +1,40 @@
 # python3
 
 
+def heap_sort(data, i, swaps):
+    
+    n = len(data)
+    parent = i
+    #nosaka vai kreisais "Child" atbilst nosacijumiem
+    krchild =2*i+1
+
+    if krchild <= (n-1) and data[krchild] < data[parent]:
+        parent = krchild
+    #nosaka vai labais "Child" atbild noscijumiem
+
+    labchild =2*i+2  
+    if labchild <= (n-1) and data[labchild] < data[parent]:
+        parent = labchild
+    if i != parent:
+        #samaina 
+        data[i], data[parent] = data[parent], data[i]
+        #ievieto apmainu "swap"
+        swaps.append((i, parent))
+        #loopo lidz noteikts 
+        heap_sort(data, parent, swaps)
+
 def build_heap(data):
     swaps = []
+    n = len(data)
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
 
-
+    for i in range(n // 2, -1, -1):
+        heap_sort(data, i, swaps)
     return swaps
+
+
+
 
 
 def main():
@@ -20,6 +47,7 @@ def main():
     # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
+    # print(data)
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
@@ -34,6 +62,8 @@ def main():
 
     # output all swaps
     print(len(swaps))
+    # print(swaps)
+    # print(data)
     for i, j in swaps:
         print(i, j)
 
